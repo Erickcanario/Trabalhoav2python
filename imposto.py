0cb5c710bf85c0c516c697eed4d3e253c208213c
Erickcanario <115334515+Erickcanario@users.noreply.github.com>
2022-11-04 20:25:37 -0300

    Add files via upload

---
+++ imposto.py
@@ -0,0 +1,7 @@
+def Imposto(taxa, Custo):
+    return (1 + taxa/100)*Custo
+    
+taxa = float(input("Digite o imposto: "))
+custo = float(input("Custo: "))
+
+print("O imposto é:", Imposto(taxa,custo))
